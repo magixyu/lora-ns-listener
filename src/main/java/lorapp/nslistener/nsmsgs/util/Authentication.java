3fd92af7d9daf0c3011b96c161b19ccd66b6567f
MoseChen <864706976@qq.com>
2016-12-20 23:01:17 +0800

    Comment unecessary main method to make sure gradle build successfully

--- src/main/java/lorapp/nslistener/nsmsgs/util/Authentication.java
+++ src/main/java/lorapp/nslistener/nsmsgs/util/Authentication.java
@@ -9,4 +9,10 @@ public class Authentication {
 	public static String generateChallenge() {
 		return "ABCDEF1234567890ABCDEF1234567890";
 	}
+
+/*	public static void main(String[] args){
+		long appEUI = 0XAA555A0000000000L;
+		long appNonce = 1234L;
+		System.out.println(appEUI|appNonce|0);
+	}*/
 }
